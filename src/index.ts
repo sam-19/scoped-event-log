2ad0739a20c23d46954a8851608795ef337314a3
sam-19 <26563023+sam-19@users.noreply.github.com>
2025-04-01 06:54:35 +0300

    Remove inspector from index This avoids side-effect of importing and compilating inspector dependencies when only log is used
    Signed-off-by: sam-19 <26563023+sam-19@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,9 @@ import {
     type LogEventProps,
     type LogEventTimestamp,
 } from './Log'
-import {
-    LogInspector,
-} from './LogInspector'
 
 export {
     Log,
-    LogInspector,
     type LogLevel,
     type LogEventListener,
     type LogEventProps,
